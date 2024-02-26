ed0d3d0010404a1cead3032c83c0427d12d17667
Dan Morris <dan@morris2morris.com>
2024-02-26 16:46:09 -0500

    add consumer projects

---
+++ ConsoleNet7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Echo;
+
+var message = "This is my message. There are many like it, but this one is mine.";
+var echoer = new Echoer();
+var echoResponse = echoer.Echo(message);
+Console.WriteLine($"Original Message: {message}");
+Console.WriteLine($"Echo response: {echoResponse}");
